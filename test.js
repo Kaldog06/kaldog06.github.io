f360624e2fa53625ad545a0e19c8c117a3a5dabf
Kaldog06 <90544621+Kaldog06@users.noreply.github.com>
2023-11-28 18:43:30 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,1 @@
-test = "https://discord.com/api/webhooks/1179229659712471130/IHMDcNU3Lx6vPi_bCsYUlAVCQwOgaK4Ito5qog8-KIXIhWgZPEnM6NENzvotYCrRXx_k";
-
-console.log(test);
+console.log("Why are you looking here?")
