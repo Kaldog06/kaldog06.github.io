5e20f625e7109c833642ac3eeef5a08c1d49d550
Kaldog06 <90544621+Kaldog06@users.noreply.github.com>
2023-11-28 18:11:25 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-test = "https://discord.com/api/webhooks/1179226475174580254/PIH8VmLcVhPLg9ruP6YRTla-wMMsOPT3rsRnqFkW76u1rK3cspJUEkk1YmRDdivOXWKd";
+test = "https://discord.com/api/webhooks/1179228033463353394/wpEncNVLsd2M-XXxYH2m8dsXuLzVbhYPWpKw3sx3F-UC4HM760udjzEyScGW3x57XwbY";
 
 console.log(test);
