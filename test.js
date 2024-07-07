a69ce838f07fe8be6d1c24aa160a0863acd7a029
Kaldog <90544621+Kaldog06@users.noreply.github.com>
2024-07-07 06:00:17 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,1 @@
 console.log("Why are you looking here?")
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-	coll[i].addEventListener("click", function() {
-		this.classList.toggle("active");
-		var content = this.nextElementSibling;
-		if (content.style.display === "block") {
-			content.style.display = "none";
-		} else {
-			content.style.display = "block";
-            	}
-	});
-}
